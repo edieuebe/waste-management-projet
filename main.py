58f950af3512d491b01657ab74ab20cc24c5b291
Gavin Jakubik <gjakubik@nd.edu>
2020-06-22 22:10:34 -0700

    Added csv file reading functionality

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 #!/usr/local/bin/python3
 
-from Can import Can
+from Drink import Drink
+import csv
+import os
 
-drinks = []
+def usage(exit_code = 0):
+    progname = os.path.basename(sys.argv[0])
+    print(f'''Usage: {progname} [-s DRINKNAME -i PATH]
+    -s DRINKNAME Singular drink data, pass the name as DRINKNAME
+    -i PATH      Specify path to data file (default drink-setup.txt)''')
+    sys.exit(exit_code)
 
-drink1 = Drink()
-drink2 = Drink()
+def main():
+    csv_reader = csv.reader(open('drink-setup.txt', 'r'))
 
-drink1.name = 'margarita'
+    # Dictionary comprehension of form {key:value for [name, cases, bottled] in csvfile}
+    drinks = {line[0]:Drink(line[0], line[1], line[2]) for line in csv_reader}
 
-drinks.append(drink1)
+    for drink in drinks.values():
+       print(f'{drink.name} : {drink.cases}')
 
-print(f'{drinks[0].name}')
+if __name__ == '__main__':
+    main()
